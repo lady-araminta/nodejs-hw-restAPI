4cd075082373db5a3e7d59a2771df19f162f0390
lady-araminta <jparkhomenko@gmail.com>
2023-03-18 16:50:00 +0200

    login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,8 +1,27 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
 
+const { SECRET_KEY } = process.env;
+
 const login = async (req, res) => {
   const { email, password } = req.body;
+  const findUser = await User.findOne({ email });
+  const passwordCompare = bcrypt.compare(password, findUser.password);
+  if (!findUser || !passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const payload = {
+    id: findUser._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  await User.findByIdAndUpdate(findUser._id, { token });
+  res.json({
+    findUser,
+    token,
+  });
 };
 
 module.exports = login;
